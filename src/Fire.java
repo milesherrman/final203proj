20b6d3aeeb780740bbb4c7521d8e8a3d3d5dfeba
Kyle Lew <kyle.lew.2002@gmail.com>
2022-12-02 12:55:09 -0800

    added fire entity

---
+++ src/Fire.java
@@ -0,0 +1,31 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public class Fire implements Animatable{
+    private final String id;
+    private Point position;
+    private final List<PImage> images;
+    private int imageIndex;
+    private final double animationPeriod;
+
+    public Fire(String id, Point position, List<PImage> images, int imageIndex, double animationPeriod) {
+        this.id = id;
+        this.position = position;
+        this.images = images;
+        this.imageIndex = imageIndex;
+        this.animationPeriod = animationPeriod;
+    }
+
+    public void scheduleActions(EventScheduler scheduler, WorldModel world, ImageStore imageStore) {
+        scheduler.scheduleEvent(this, createAnimationAction(0), animationPeriod);
+    }
+
+    public double getAnimationPeriod() {return this.animationPeriod;}
+    public void setImgIdx(int value) {imageIndex = value;}
+    public String getEntityID(){return this.id;}
+    public Point getEntityPos(){return this.position;}
+    public void setEntityPos(Point point){this.position = point;}
+    public List<PImage> getEntityImage(){return this.images;}
+    public int getEntityImgIdx(){return this.imageIndex;}
+}
