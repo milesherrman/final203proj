bce20d4c05a3893460c5f67028b06c55b2935794
Kyle Lew <kyle.lew.2002@gmail.com>
2022-12-06 14:15:20 -0800

    added dude_fire and burnt grass images

---
+++ src/Dude_Fire.java
@@ -0,0 +1,42 @@
+import processing.core.PImage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Dude_Fire implements Moveable{
+    private final String id;
+    private Point position;
+    private final List<PImage> images;
+    private int imageIndex;
+    private final double actionPeriod;
+    private final double animationPeriod;
+
+    public Dude_Fire(String id, Point position, List<PImage> images, double actionPeriod, double animationPeriod) {
+        this.id = id;
+        this.position = position;
+        this.images = images;
+        this.imageIndex = 0;
+        this.actionPeriod = actionPeriod;
+        this.animationPeriod = animationPeriod;
+    }
+
+
+    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
+    }
+
+    public double getActionPeriod() { return actionPeriod; }
+    public void setImgIdx(int value) {imageIndex = value;}
+    public double getAnimationPeriod() { return this.animationPeriod; }
+    public String getEntityID(){ return this.id; }
+    public Point getEntityPos(){return this.position;}
+    public void setEntityPos(Point point){this.position = point;}
+    public List<PImage> getEntityImage(){return this.images;}
+    public int getEntityImgIdx(){return this.imageIndex;}
+    public boolean _targetReached(WorldModel world, Point target, EventScheduler scheduler) {
+        return true;
+    }
+    public boolean _stumpCheck(WorldModel world, Point newPos) {
+        return world.getOccupancyCell(newPos).getClass() != Stump.class;
+    }
+}
